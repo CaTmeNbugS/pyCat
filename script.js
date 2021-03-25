89cd88374f1baa4f5e5d5740709bb41ebd1f022a
CaTmeNbugS <catmen826@gmail.com>
2021-03-25 19:06:35 +0500

    Close animation

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-search = document.querySelector('.searchContainer');
-menu = document.querySelector('.menuContainer');
-width = document.querySelector('body');
-label = document.querySelector('.labelContainer');
-navMenu = document.querySelector('.navMenu');
+search = document.querySelector(".searchContainer");
+menu = document.querySelector(".menuContainer");
+width = document.querySelector("body");
+label = document.querySelector(".labelContainer");
+navMenu = document.querySelector(".navMenu");
 
-search.style.marginLeft = width.offsetWidth / 2 - menu.offsetWidth - label.offsetWidth - search.offsetWidth / 2 + `px`;
+search.style.marginLeft =
+  width.offsetWidth / 2 -
+  menu.offsetWidth -
+  label.offsetWidth -
+  search.offsetWidth / 2 +
+  `px`;
 
-menu.addEventListener('click', function(){
-    navMenu.classList.toggle('show');
-})
+menu.addEventListener("click", function () {
+  line = document.querySelectorAll(".chtMenu");
+  navMenu.classList.toggle("show");
+  line[0].classList.toggle("close");
+  line[2].classList.toggle("Close");
+  line[1].classList.toggle("Close");
+});
 
-const buttons = document.querySelectorAll('.btn');
-    buttons.forEach(btn => {
-        btn.addEventListener('click', function(event){
+const buttons = document.querySelectorAll(".ripplrEffect");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    let x = event.clientX;
+    let y = event.clientY - btn.offsetTop;
 
-            let x = event.clientX;
-            let y = event.clientY - btn.offsetTop;
+    let ripple = document.createElement("span");
+    ripple.style.left = x + `px`;
+    ripple.style.top = y + `px`;
+    ripple.classList.add("ripple");
+    this.appendChild(ripple);
 
-            let ripple = document.createElement('span');
-            ripple.style.left = x + `px`;
-            ripple.style.top = y + `px`;
-            ripple.classList.add('ripple');
-            this.appendChild(ripple);
-
-            setTimeout(() =>{
-                ripple.remove()
-            },1000)
-        })
-    })
+    setTimeout(() => {
+      ripple.remove();
+    }, 1000);
+  });
+});
