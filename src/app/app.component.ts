09cfe9f84cfd1ab6006b336e063c7ce6e4d9ec6a
CaTmeNbugS <catmen826@gmail.com>
2021-06-12 08:50:28 +0500

    Анимация для nv_bar'а

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { trigger, state, style, transition, animate, query } from '@angular/animations';
 import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {slider} from './route-animations';
@@ -9,6 +10,21 @@ import {slider} from './route-animations';
   styleUrls: ['./app.component.scss'],
   animations: [
     slider,
+    trigger('side_bar', [
+      state('n_spr', style({width: '80px'})),
+      state('spr', style({width: '280px'})),
+      transition('n_spr => spr', animate('.2s ease-out')
+      ),
+      transition('spr => n_spr', animate('.2s ease-out')),
+
+    ]),
+    trigger('nv_text',[
+      state('hidden', style({opacity: 0, visibility: 'hidden'})),
+      state('visible', style({opacity: 1, visibility: 'visible' })),
+      transition('hidden => visible', animate('.1s')),
+      transition('visible => hidden', animate('.1s')),
+
+    ])
   ]
 })
 export class AppComponent implements OnInit {
@@ -37,11 +53,11 @@ export class AppComponent implements OnInit {
   }
   constructor() {
   }
-  spreadM = false;
+  side_bar_state = 'n_spr';
+  btn_text_state = 'hidden';
   spreadMenu(){
-    const bar = document.querySelector<HTMLElement>('.nv_bar')
-    bar.classList.toggle('spr')
-    this.spreadM = !this.spreadM;
+    this.side_bar_state = this.side_bar_state === 'n_spr' ? 'spr' : 'n_spr'
+    this.btn_text_state = this.btn_text_state === 'hidden' ? 'visible' : 'hidden'
   }
 
   prepareRoute(outlet: RouterOutlet){
